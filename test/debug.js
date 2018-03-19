a0e1ad3c00078085244836d3270f012561992487
Vladimir Agafonkin <agafonkin@gmail.com>
2018-03-19 16:10:53 +0200

    Switch to Flatbush for indexing (#9)
    * rewrite with flatbush and flat arrays
    
    * minor fixes
    
    * upgrade flatbush

--- test/debug.js
+++ test/debug.js
@@ -4,12 +4,14 @@ var linematch = require('../');
 
 var lines1 = require('./fixtures/tiger1');
 var lines2 = require('./fixtures/osm1');
+var result = require('./fixtures/diff1.json');
 
-// console.time('match');
+console.time('match');
+for (var i = 0; i < 1000; i++)
 var result = linematch(lines1, lines2, 0.0001);
-// console.timeEnd('match');
+console.timeEnd('match');
 
-console.log(JSON.stringify(result));
+// console.log(JSON.stringify(result));
 
 var geojson = {
     type: 'FeatureCollection',
